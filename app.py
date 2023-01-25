7629cfa1f008ada23933e423a5418ae9e01b87f5
jaysys <jaysys@gmail.com>
2023-01-25 23:13:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -83,29 +83,32 @@ def meta():
 def bokeh():
     # init a basic bar chart:
     # http://bokeh.pydata.org/en/latest/docs/user_guide/plotting.html#bars
-    fig = figure(width=600, height=600)
-    fig.vbar(
-        x=[1, 2, 3, 4],
-        width=0.5,
-        bottom=0,
-        top=[1.7, 2.2, 4.6, 3.9],
-        color='navy'
-    )
 
-    # grab the static resources
-    js_resources = INLINE.render_js()
-    css_resources = INLINE.render_css()
+    # fig = figure(width=600, height=600)
+    # fig.vbar(
+    #     x=[1, 2, 3, 4],
+    #     width=0.5,
+    #     bottom=0,
+    #     top=[1.7, 2.2, 4.6, 3.9],
+    #     color='navy'
+    # )
 
-    # render template
-    script, div = components(fig)
-    html = render_template(
-        'bokeh.html',
-        plot_script=script,
-        plot_div=div,
-        js_resources=js_resources,
-        css_resources=css_resources,
-    )
-    return (html)
+    # # grab the static resources
+    # js_resources = INLINE.render_js()
+    # css_resources = INLINE.render_css()
+
+    # # render template
+    # script, div = components(fig)
+    # html = render_template(
+    #     'bokeh.html',
+    #     plot_script=script,
+    #     plot_div=div,
+    #     js_resources=js_resources,
+    #     css_resources=css_resources,
+    # )
+    # return (html)
+    
+    pass
 
 
 if __name__ == "__main__":
