8e25b6f220b59cda2be4621e15c3eeb6d5035bca
jaysys <jaysys@gmail.com>
2023-01-25 22:58:06 +0900

    update

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+from bokeh.embed import components
+from bokeh.plotting import figure
+from bokeh.resources import INLINE
+
 try:
     db_conn = os.environ.get('DBCONN')
     print(db_conn)
@@ -73,6 +77,35 @@ def update(id):
 @app.route('/meta')
 def meta():
     return render_template('meta.html')
+
+
+@app.route('/bokeh')
+def bokeh():
+    # init a basic bar chart:
+    # http://bokeh.pydata.org/en/latest/docs/user_guide/plotting.html#bars
+    fig = figure(width=600, height=600)
+    fig.vbar(
+        x=[1, 2, 3, 4],
+        width=0.5,
+        bottom=0,
+        top=[1.7, 2.2, 4.6, 3.9],
+        color='navy'
+    )
+
+    # grab the static resources
+    js_resources = INLINE.render_js()
+    css_resources = INLINE.render_css()
+
+    # render template
+    script, div = components(fig)
+    html = render_template(
+        'bokeh.html',
+        plot_script=script,
+        plot_div=div,
+        js_resources=js_resources,
+        css_resources=css_resources,
+    )
+    return (html)
 
 
 if __name__ == "__main__":
