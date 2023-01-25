1cc1ed47e13ad1fb461216fcb82907da15613f7f
jaysys <jaysys@gmail.com>
2023-01-25 23:20:24 +0900

    update

--- app.py
+++ app.py
@@ -84,32 +84,30 @@ def bokeh():
     # init a basic bar chart:
     # http://bokeh.pydata.org/en/latest/docs/user_guide/plotting.html#bars
 
-    # fig = figure(width=600, height=600)
-    # fig.vbar(
-    #     x=[1, 2, 3, 4],
-    #     width=0.5,
-    #     bottom=0,
-    #     top=[1.7, 2.2, 4.6, 3.9],
-    #     color='navy'
-    # )
+    fig = figure(width=600, height=600)
+    fig.vbar(
+        x=[1, 2, 3, 4],
+        width=0.5,
+        bottom=0,
+        top=[1.7, 2.2, 4.6, 3.9],
+        color='navy'
+    )
 
-    # # grab the static resources
-    # js_resources = INLINE.render_js()
-    # css_resources = INLINE.render_css()
+    # grab the static resources
+    js_resources = INLINE.render_js()
+    css_resources = INLINE.render_css()
 
-    # # render template
-    # script, div = components(fig)
-    # html = render_template(
-    #     'bokeh.html',
-    #     plot_script=script,
-    #     plot_div=div,
-    #     js_resources=js_resources,
-    #     css_resources=css_resources,
-    # )
-    # return (html)
+    # render template
+    script, div = components(fig)
+    html = render_template(
+        'bokeh.html',
+        plot_script=script,
+        plot_div=div,
+        js_resources=js_resources,
+        css_resources=css_resources,
+    )
+    return (html)
     
-    pass
-
 
 if __name__ == "__main__":
     app.run(debug=True)
