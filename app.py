f523cf4059a23c3684cc2fd48b80061a6fadecf7
jaysys <jaysys@gmail.com>
2023-01-26 22:39:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -186,12 +186,12 @@ def dfbokeh():
     ax = pd.to_datetime(df_div["date"])
     ay = df_div['total'] 
     fig.circle(ax, ay , size=1, color="black", alpha=1)#, x_axis_type="datetime")
-    fig.yaxis[0].formatter = NumeralTickFormatter(format="0")
+    fig.yaxis[0].formatter = NumeralTickFormatter(format="0,0")
     fig.xaxis[0].formatter = DatetimeTickFormatter(months="%F")
     #fig.xgrid.grid_line_color = "olive"
     fig.ygrid.band_fill_color = "olive"
     fig.ygrid.band_fill_alpha = 0.1   
-    print(ax)
+    #print(ax)
     # grab the static resources
     js_resources = INLINE.render_js()
     css_resources = INLINE.render_css()
