cd08b0e1009fbb96065183a3aa6923a506d99460
jaysys <jaysys@gmail.com>
2023-01-27 23:55:22 +0900

    Update env.py

--- env.py
+++ env.py
@@ -18,12 +18,12 @@ db = create_engine(db_conn)
 conn = db.connect()
 print("[db_connection~]",db,conn)
 
-qry000="select * from my_asset"
+qry_all="select * from my_asset"
 
 qry_div_sum = "select to_char(timestamp::timestamp,'YYYY/Mon/DD/HH24:MI') as date, div, round(sum(total_krw)) as total_krw from my_asset group by div, timestamp order by timestamp desc"
 
-qry002 = "SELECT timestamp as date, round(sum(total_krw)) as total FROM my_asset GROUP BY timestamp ORDER BY timestamp desc"
+qry_crypto = "select to_char(timestamp::timestamp,'YYYY/Mon/DD/HH24:MI') as date, div, round(sum(total_krw)) as total_krw from my_asset where div = 'CRYPTO' group by div, timestamp order by timestamp desc"
 
-res = pd.read_sql(qry_div_sum, conn, index_col=None)
+res = pd.read_sql(qry_crypto, conn, index_col=None)
 print(res)#.to_markdown(floatfmt=',.2f'))
 
